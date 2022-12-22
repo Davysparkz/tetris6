e872ffcd5ca47147ef69d61f7e821f3d40f782bd
Davysparkz <dabakura@gmail.com>
2022-12-22 14:04:33 +0100

    Created WIN32 type aliases

---
+++ tetris6/w32_types.h
@@ -0,0 +1,24 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+
+namespace w32 {
+	using hinstance_t = HINSTANCE;
+	using wchar = WCHAR;
+	using atom_t = ATOM;
+	using wparam_t = WPARAM;
+	using lparam_t = LPARAM;
+	using uint_t = UINT;
+	using lpwstr_t = LPWSTR;
+	using bool_t = BOOL;
+	using lresult_t = LRESULT;
+	using haccel_t = HACCEL;
+	using msg_t = MSG;
+	using wndclassexw_t = WNDCLASSEXW;
+	using wndclass_t = WNDCLASSEX;
+	using hbrush_t = HBRUSH;
+	using hwnd_t = HWND;
+	using paintstruct_t = PAINTSTRUCT;
+	using hdc_t = HDC;
+}

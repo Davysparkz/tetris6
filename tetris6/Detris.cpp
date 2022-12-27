0d839cb0773e24ad021ce522aa47dac58935ab1b
Davysparkz <dabakura@gmail.com>
2022-12-27 17:24:28 +0100

    Added more convenience methods/functionality

--- tetris6/Detris.cpp
+++ tetris6/Detris.cpp
@@ -2,11 +2,14 @@
 #include "Detris.h"
 #include "TGraphics.h"
 
-Detris::Detris(uint_t x, uint_t y, colorref_t color)
+Detris::Detris(uint_t x, uint_t y, uint_t cellSize, colorref_t color)
 {
 	this->cell.X = x;
 	this->cell.Y = y;
 	this->color = color;
+	this->touched_down = false;
+	this->cell.LocY = 1;
+	this->cell.Size = cellSize;
 }
 
 
@@ -16,17 +19,66 @@ Detris::~Detris()
 
 void Detris::MoveLeft()
 {
+	if (this->touched_down) return;
 	this->cell.X -= this->cell.Size;
+	this->cell.LocX = this->cell.X / this->cell.Size;
 }
 
 void Detris::MoveRight()
 {
+	if (this->touched_down) return;
 	this->cell.X += this->cell.Size;
+	this->cell.LocX = this->cell.X / this->cell.Size;
 }
 
 void Detris::MoveDown()
 {
+	if (this->touched_down) return;
 	this->cell.Y += this->cell.Size;
+	this->cell.LocY = (this->cell.Y / this->cell.Size) - 3;
+}
+
+bool Detris::TouchedDown() {
+	return touched_down;
+}
+
+void Detris::TouchDown(bool td) {
+	this->touched_down = td;
+}
+
+uint_t Detris::GetXLoc()
+{
+	return this->cell.LocX;
+}
+
+uint_t Detris::GetYLoc()
+{
+	return this->cell.LocY;
+}
+
+void Detris::SetXLoc(uint_t xLoc)
+{
+	this->cell.LocX = xLoc;
+}
+
+void Detris::SetYLoc(uint_t yLoc)
+{
+	this->cell.LocY = yLoc;
+}
+
+uint_t Detris::GetX()
+{
+	return this->cell.X;
+}
+
+uint_t Detris::GetY()
+{
+	return this->cell.Y;
+}
+
+void Detris::SetY(uint_t y)
+{
+	this->cell.Y = y;
 }
 
 void Detris::Draw(hdc_t hdc)
@@ -41,26 +93,32 @@ void Detris::Draw(hdc_t hdc)
 	);
 }
 
-bool InLeft(const Detris & d, uint_t left)
+bool InLeft(const Detris * d, uint_t left)
 {
-	if (d.cell.X <= left) {
-		return true;
+	if (d) {
+		if (d->cell.X <= left) {
+			return true;
+		}
 	}
 	return false;
 }
 
-bool InRight(const Detris & d, uint_t right)
+bool InRight(const Detris * d, uint_t right)
 {
-	if ((d.cell.X+d.cell.Size) >= right) {
-		return true;
+	if (d) {
+		if ((d->cell.X + d->cell.Size) >= right) {
+			return true;
+		}
 	}
 	return false;
 }
 
-bool InBottom(const Detris & d, uint_t bottom)
+bool InTop(const Detris * d, uint_t top)
 {
-	if ((d.cell.Y + d.cell.Size) >= bottom) {
-		return true;
+	if (d) {
+		if ((d->cell.Y + d->cell.Size) >= top) {
+			return true;
+		}
 	}
 	return false;
 }
--- tetris6/Detris.h
+++ tetris6/Detris.h
@@ -7,9 +7,14 @@ using namespace w32;
 
 struct DCell {
 	//std::pair<uint_t, uint_t> Index;
-	const uint_t Size{ 44 };
+	// SINCE THIS REPRESENTS A SQUARE, THE SIZE MUST BE SAME
+	uint_t Size;
+	// PIXEL UNITS
 	uint_t X;
 	uint_t Y;
+	// GRID  UNITS
+	uint_t LocX{ 1 };
+	uint_t LocY{ 1 };
 	bool Marked;
 };
 
@@ -21,7 +26,7 @@ class Detris
 {
 public:
 	Detris() = delete;
-	Detris(uint_t x, uint_t y, colorref_t color);
+	Detris(uint_t x, uint_t y, uint_t cellSize, colorref_t color);
 	~Detris();
 
 	void MoveLeft();
@@ -30,11 +35,27 @@ public:
 
 	void Draw(hdc_t hdc);
 
-	friend bool InLeft(const Detris& d, uint_t left);
-	friend bool InRight(const Detris& d, uint_t right);
-	friend bool InBottom(const Detris& d, uint_t bottom);
+	friend bool InLeft(const Detris* d, uint_t left);
+	friend bool InRight(const Detris* d, uint_t right);
+	friend bool InTop(const Detris* d, uint_t top);
+
+	bool TouchedDown();
+	void TouchDown(bool);
+
+
+	uint_t GetXLoc();
+	uint_t GetYLoc();
+	void SetXLoc(uint_t xLoc);
+	void SetYLoc(uint_t yLoc);
+
+	uint_t GetX();
+	uint_t GetY();
+
+	// USEFUL FOR FAST DROPPING THE DETRIS
+	void SetY(uint_t y);
 private:
 	DCell cell;
 	colorref_t color;
+	bool touched_down;
 };
 
